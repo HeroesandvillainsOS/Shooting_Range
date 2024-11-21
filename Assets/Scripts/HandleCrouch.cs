28a24ac536e9d52cdd8831a048f5cedd2f905400
HeroesandvillainsOS <Beherenow119@gmail.com>
2024-11-21 12:26:12 -0600

    Ensures the player collider's height and camera position change when crouching.

--- Assets/Scripts/HandleCrouch.cs
+++ Assets/Scripts/HandleCrouch.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class HandleCrouch : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Crouching
+    private float cameraStandHeight = 0.7f;
+    private float cameraCrouchHeight = 0.4f;
+    private float capsuleStandHeight = 2f;
+    private float capsuleCrouchHeight = 1.5f;
+
+    // Lerp smoothness
+    private float smoothAmount = 1f;
+
+    // Game Objects
+    private PlayerController playerController;
+    private CapsuleCollider capsuleCollider;
+
     void Start()
     {
-        
+        // Gets reference to Game Object Components
+        playerController = GetComponentInParent<PlayerController>();
+        capsuleCollider = GetComponentInParent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 currentPosition = transform.localPosition;
+
+        // Handles crouching
+        if (playerController.isCrouching)
+        {
+            // Smoothly lerp the camera position to crouch height
+            float cameraCrouchLerp = Mathf.Lerp(cameraStandHeight, cameraCrouchHeight, smoothAmount);
+            transform.localPosition = new Vector3(currentPosition.x, cameraCrouchLerp , currentPosition.z);
+
+            // Smoothly lerp the capsule collider height to crouch height
+            capsuleCollider.height = Mathf.Lerp(capsuleStandHeight, capsuleCrouchHeight, smoothAmount);
+
+        }
+        // Handles standing
+        else
+        {
+            // Smoothly lerp the camera position to stand height
+            float cameraStandLerp = Mathf.Lerp(cameraCrouchHeight, cameraStandHeight, smoothAmount);
+            transform.localPosition = new Vector3(currentPosition.x, cameraStandLerp, currentPosition.z);
+
+            // Smoothly lerp the capsule collider height to stand height
+            capsuleCollider.height = Mathf.Lerp(capsuleCrouchHeight, capsuleStandHeight, smoothAmount);
+        }
     }
 }
