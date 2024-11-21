c262e7f013b82435de533320947d579ce25e7459
HeroesandvillainsOS <Beherenow119@gmail.com>
2024-11-20 21:10:59 -0600

    Begins the process of enabling crouching.

--- Assets/Scripts/LookUpAndDown.cs
+++ Assets/Scripts/LookUpAndDown.cs
@@ -1,28 +1,61 @@
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class LookUpAndDown : MonoBehaviour
 {
-    private float mouseSensitivity = 50f;
+    // Rotation
+    private float mouseSensitivity = 0.1f;
     private float xRotation = 0f;
     private float maxRotation = 45f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Crouching variables
+    //private float standHeight = 1.7f;
+    //private float crouchHeight = 1.3f;
+    private float smoothTransitionTime = 10f;
+
+    // Input System
+    private Vector2 lookDirection;
+    public InputActionReference look;
+
+    // Reference for the PlayerController script
+    private PlayerController playerController;
+
+
+    private void Start()
     {
-        
+        // Creates an object of the PlayerController component
+        playerController = GetComponentInParent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        // Reads the Input System moveDirection Vector 2 value
+        lookDirection = look.action.ReadValue<Vector2>();
+
+        // Gets the player object's height
+        float playerHeight = transform.parent.position.y;
+        float heightDifference = playerHeight - transform.position.y; // 1.7, 1.0 = .07
+        float standHeight = playerHeight - heightDifference;
+        Debug.Log($"Height Diffence {heightDifference} Stand Height {standHeight}");
+        float crouchHeight = (playerHeight - heightDifference) - 0.1f;
+
+        // Sets the crouch height and ensures a smooth transition
+        Vector3 currentPosition = transform.position;
+        float targetHeight = playerController.isCrouching ? crouchHeight : standHeight;
+        float smoothHeight = Mathf.Lerp(transform.position.y, targetHeight, Time.deltaTime * smoothTransitionTime);
+        transform.position = new Vector3(currentPosition.x, smoothHeight, currentPosition.z);
     }
 
     // Ensures vertical rotation is calculated after horizontal rotation
     void LateUpdate()
     {
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseY = lookDirection.y * mouseSensitivity;
 
-        // Inverts the horizontal mouse movements
+        // Inverts the vertical mouse movements
         xRotation -= mouseY;
-        // Limits horizontal rotation
+        // Limits vertical rotation
         xRotation = Mathf.Clamp(xRotation, -maxRotation, maxRotation);
-
-
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
     }
 }
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     // Rotation
-    private float mouseSensitivity = 100f;
+    private float mouseSensitivity = 0.2f;
     private float yRotation = 0f; 
       
     // Movement
@@ -13,49 +14,74 @@ public class PlayerController : MonoBehaviour
     private float jogSpeed = 8f;
 
     // Bools
-    private bool isCrouching = false;
+    public bool isCrouching = false;
     private bool isJogging = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    // Input System
+    private Vector2 moveDirection;
+    private Vector2 lookDirection;
+    // Assign controls in the Inspector
+    public InputActionReference move;
+    public InputActionReference look;
+    public InputActionReference jog;
+    public InputActionReference crouch;
 
+    private void OnEnable()
+    {
+        // Subscribes to Button event methods
+        jog.action.started += Jog;
+        jog.action.canceled += StopJog;
+        crouch.action.started += Crouch;
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribes to the Button event methods
+        jog.action.started -= Jog;
+        jog.action.canceled -= StopJog;
+        crouch.action.started -= Crouch;
+    }
+
+    private void Update()
+    {
+        // Reads the Input System moveDirection Vector 2 value
+        moveDirection = move.action.ReadValue<Vector2>();
+        lookDirection = look.action.ReadValue<Vector2>();
     }
 
     // Ensures there is no stuttering when colliding with objects
     void FixedUpdate()
     {
-        // Stores the values of the user's inputs
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        // Stores the values of the user's inputs     
+        float mouseX = lookDirection.x * mouseSensitivity;
        
         // Rotation
         yRotation += mouseX;      
         transform.rotation = Quaternion.Euler(0, yRotation, 0);
        
-        // Movement
-        // Checks to see if the player is jogging or crouching
-        if (Input.GetKey(KeyCode.C) == true)
-            isCrouching = true;
-        else if (Input.GetKey(KeyCode.LeftShift) == true)
-        {
-            isJogging = true;
-            isCrouching = false;
-        }
-        else if (!Input.GetKey(KeyCode.C) && !Input.GetKey(KeyCode.LeftShift))
-        {
-            isCrouching = false;
-            isJogging = false;
-        }
-
         // Sets the speed based on whether the player is jogging or crouching
         speed = isCrouching ? crouchSpeed : isJogging ? jogSpeed : walkSpeed;
 
         // Normalizes the movement so moving diagonal isn't faster than non-diagonal movements
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        Vector3 movement = new Vector3(moveDirection.x, 0f, moveDirection.y).normalized;
 
         // Moves the player
         transform.Translate(speed * Time.deltaTime * movement);
+    }
+
+    // Methods that handle button events
+    private void Jog(InputAction.CallbackContext context)
+    {
+        isJogging = true;
+    }
+
+    private void StopJog(InputAction.CallbackContext context)
+    {
+        isJogging = false;
+    }
+
+    private void Crouch(InputAction.CallbackContext context)
+    {
+        isCrouching = !isCrouching;
     }
 }
