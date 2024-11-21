f890ac7fcdd64fe315fa66ed92852531234ae454
HeroesandvillainsOS <Beherenow119@gmail.com>
2024-11-21 08:27:00 -0600

    Preps the program for crouching to be handled in its own script.

--- Assets/Scripts/LookUpAndDown.cs
+++ Assets/Scripts/LookUpAndDown.cs
@@ -8,42 +8,14 @@ public class LookUpAndDown : MonoBehaviour
     private float xRotation = 0f;
     private float maxRotation = 45f;
 
-    // Crouching variables
-    //private float standHeight = 1.7f;
-    //private float crouchHeight = 1.3f;
-    private float smoothTransitionTime = 10f;
-
     // Input System
     private Vector2 lookDirection;
     public InputActionReference look;
-
-    // Reference for the PlayerController script
-    private PlayerController playerController;
-
-
-    private void Start()
-    {
-        // Creates an object of the PlayerController component
-        playerController = GetComponentInParent<PlayerController>();
-    }
 
     private void Update()
     {
         // Reads the Input System moveDirection Vector 2 value
         lookDirection = look.action.ReadValue<Vector2>();
-
-        // Gets the player object's height
-        float playerHeight = transform.parent.position.y;
-        float heightDifference = playerHeight - transform.position.y; // 1.7, 1.0 = .07
-        float standHeight = playerHeight - heightDifference;
-        Debug.Log($"Height Diffence {heightDifference} Stand Height {standHeight}");
-        float crouchHeight = (playerHeight - heightDifference) - 0.1f;
-
-        // Sets the crouch height and ensures a smooth transition
-        Vector3 currentPosition = transform.position;
-        float targetHeight = playerController.isCrouching ? crouchHeight : standHeight;
-        float smoothHeight = Mathf.Lerp(transform.position.y, targetHeight, Time.deltaTime * smoothTransitionTime);
-        transform.position = new Vector3(currentPosition.x, smoothHeight, currentPosition.z);
     }
 
     // Ensures vertical rotation is calculated after horizontal rotation
@@ -55,6 +27,7 @@ public class LookUpAndDown : MonoBehaviour
         xRotation -= mouseY;
         // Limits vertical rotation
         xRotation = Mathf.Clamp(xRotation, -maxRotation, maxRotation);
+        // Initiates vertical rotation
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
     }
 }
